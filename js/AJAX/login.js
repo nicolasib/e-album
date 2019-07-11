5246deb47cd776e8b0296fa89fc9566ea57bcc79
joaovitormelo <49257395+joaovitormelo@users.noreply.github.com>
2019-07-11 17:16:12 -0300

    Fix Login

--- js/AJAX/login.js
+++ js/AJAX/login.js
@@ -2,14 +2,15 @@ $(document).ready(function () {
     var form_Data;
     $(".login-form").submit(function(e) {
         e.preventDefault();
-        var name = $('input[name=name]').val();
+        var login = $('input[name=name]').val();
         var pass = $('input[name=pass]').val();
         
         $.ajax({
             url: './php/controllers/login.php', // Url do lado server que vai receber o arquivo
-            data: {name: name, pass: pass},
+            data: {login: login, pass: pass},
             type: 'POST',
             success: function (info) {
+                window.alert(info);
                 if(info == 'Email ou senha errado!'){
                     $('.register-erro').html(info);
                 }else{
