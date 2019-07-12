cfec4327abb3dffda7a06593598768055b46c03e
joaovitormelo <49257395+joaovitormelo@users.noreply.github.com>
2019-07-12 19:05:17 -0300

    Delete Fix
    Apaga as figurinhas de um usuário antes de deletar usuário

--- pages/php/controllers/cr_delete.php
+++ pages/php/controllers/cr_delete.php
@@ -2,7 +2,8 @@
     include_once("../functions/op_users.php");
     session_start();
     $name = $_SESSION["name"];
-    $return = delete($name);
+    $id = $_SESSION["id"];
+    $return = delete($name, $id);
     if($return == 0){
         echo "Não foi possível deletar!";
     }
