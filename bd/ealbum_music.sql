ecfcbba969402ce01b0739d08e9a0f88c0342ef3
nicolasib <design.nicolasib@gmail.com>
2019-07-12 21:23:17 -0300

    Revert "Voltando essa bosta"
    This reverts commit 2dc76b029d7d0c9d19d6ce2f16db7186deb5a5a7.

--- bd/ealbum_music.sql
+++ bd/ealbum_music.sql
@@ -25,38 +25,38 @@ create table if not exists card_album(
         references album(id_album)
 );
 
-create table if not exists team(
-	id_team int not null primary key,
-    name_team varchar(45) unique not null
+create table if not exists singer(
+	id_singer int not null primary key,
+    name_singer varchar(45) unique not null
 );
 
-create table if not exists player(
-	id_team int not null,
-	id_player int not null,
-    name_player varchar(45) unique not null,
-    constraint pk_playerM primary key(id_player, id_team),
-    constraint fk_playerM foreign key(id_team)
-        references team(id_team)
+create table if not exists music(
+	id_singer int not null,
+	id_music int not null,
+    name_music varchar(45) unique not null,
+    constraint pk_musicM primary key(id_music, id_singer),
+    constraint fk_musicM foreign key(id_singer)
+        references singer(id_singer)
 );
 
-create table if not exists card_player(
+create table if not exists card_music(
     id_user int not null,
-    id_team int not null,
-    id_player int not null,
+    id_singer int not null,
+    id_music int not null,
     amount int not null,
-    constraint pk_cardCM primary key(id_user, id_team, id_player),
+    constraint pk_cardCM primary key(id_user, id_singer, id_music),
     constraint fk_userCM foreign key(id_user)
         references user(id_user),
-	constraint fk_teamCM foreign key(id_team)
-        references team(id_team),
-	constraint fk_playerCM foreign key(id_player)
-        references player(id_player)
+	constraint fk_singerCM foreign key(id_singer)
+        references singer(id_singer),
+	constraint fk_musicCM foreign key(id_music)
+        references music(id_music)
 );
 
-INSERT INTO team(id_team, name_team) VALUES
+INSERT INTO singer(id_singer, name_singer) VALUES
 	(1,"Eminem (Marshall Bruce Mathers III)");
     
-INSERT INTO player(id_team, id_player, name_player) VALUES
+INSERT INTO music(id_singer, id_music, name_music) VALUES
 	(1, 1,"Lose Yourself"),
     (1, 2,"Till I Collapse"),
     (1, 3,"The Real Slim Shady"),
